08d7e3c6884169024a8697f87e6feec89b222537
cdricn <cdricncdricn@gmail.com>
2025-12-25 14:41:20 +0800

    Adding posts scraper

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as cheerio from 'cheerio';
 import axios from 'axios';
+import type { Entries, Posts } from './interface';
 
 const PORT = 8000;
 const app = express();
@@ -8,14 +9,6 @@ const app = express();
 app.get('/', (req, res) => {
   res.json('Welcome! Go to /jams to get the list of gamejams. Go to /posts to get the list of posts per gamejam.');
 });
-
-interface Entries {
-  title: string,
-  url: string,
-  members: number,
-  deadline: string,
-  host: string
-}
 
 app.get('/jams', (req, res) => {
   axios.get('https://itch.io/jams/in-progress/ranked/with-participants')
@@ -38,17 +31,65 @@ app.get('/jams', (req, res) => {
       });
 
       res.json(entries);
+
     }).catch((err) => {
       console.log(err);
     })
 });
 
+function GetPosts($:cheerio.CheerioAPI, entries:Posts[]) {
+  $('.topic_row').each((_, element) => {
+    const $element = $(element);
+    const title = $element.find('.topic_link').text();
+    const url = 'https://itch.io' + $element.find('a').attr('href');
+    const preview = $element.find('.topic_preview').text();
+    const replies = Number($element.find('.number_value').first().text());
+    const datePosted = $element.find('.topic_date').attr('title')!;
+    const author = $element.find('.topic_author').text();
 
-/*app.get('/posts', (req, res) => {
-  entries.forEach(entry => {
-    axios.get(entry.url);
-  })
-});*/
+    entries.push({title, url, preview, replies, datePosted, author});
+  });
+  return entries;
+}
+
+async function GoToNextPage(currentPageLink:string, entries:Posts[]) {
+  let newEntries = await axios.get(currentPageLink)
+    .then((response) => { 
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const nextPageText = $('.category_pager').find('a').first().text();
+      const nextPageLink = 'https://itch.io' + $('.category_pager').find('a').attr('href');
+
+      GetPosts($, entries);
+
+      if (nextPageText==='Next page') {
+        GoToNextPage(nextPageLink, entries);
+      }
+    })
+  
+      console.log(newEntries);
+}
+
+app.get('/posts/:jamLink', async (req, res) => {
+  const jamLink = 'https://itch.io/jam/' + req.params.jamLink + '/community';
+  //let populatedEntries = CyclePages(jamLink, entries);
+
+  axios.get(jamLink)
+    .then((response) => { 
+      let entries : Posts[] = [];
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const nextPageText = $('.category_pager').find('a').first().text();
+      const nextPageLink = 'https://itch.io' + $('.category_pager').find('a').attr('href');
+
+      entries = GetPosts($, entries);
+      if (nextPageText==='Next page') {
+        GoToNextPage(nextPageLink, entries);
+      }
+      
+      res.json(entries);
+    })
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
---
+++ interface.ts
@@ -0,0 +1,17 @@
+
+export interface Entries {
+  title: string,
+  url: string,
+  members: number,
+  deadline: string,
+  host: string
+}
+
+export interface Posts {
+  title: string,
+  url: string,
+  preview: string,
+  replies: number,
+  datePosted: string,
+  author: string
+}
