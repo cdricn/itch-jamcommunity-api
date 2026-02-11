35f58d4f68733b071f2269732b227db74eeef515
cdricn <cdricncdricn@gmail.com>
2026-02-11 18:09:40 +0800

    Created keywords dictionary

---
+++ keywords.js
@@ -0,0 +1,35 @@
+export const keywords = {
+  // normal terms
+  team: "team",
+  join: "join",
+  need: "need",
+  group: "group", 
+  contribute: "contribute",
+
+  // verbs
+  looking: "looking",
+  needed: "needed",
+
+  // roles
+  va: "voice actor", 
+  actor: "actor", 
+  artist: "artist", 
+  producer: "producer", 
+  musician: "musician", 
+  coder: "coder", 
+  composer: "composer", 
+  programmer: "programmer", 
+  developer: "developer",
+
+  // plural roles
+  teams: "teams", 
+  vas: "voice actor", 
+  actors: "actor", 
+  artists: "artist", 
+  producers: "producer", 
+  musicians: "musician", 
+  coders: "coder", 
+  composers: "composer", 
+  programmers: "programmer", 
+  developers: "developer"
+}
