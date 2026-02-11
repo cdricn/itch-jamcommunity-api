071c6d8e8650dd8d34d0ad5891c1ccd816401314
cdricn <cdricncdricn@gmail.com>
2026-02-11 18:16:56 +0800

    Separated tags to its own file

---
+++ definedTags.js
@@ -0,0 +1,19 @@
+export const defined_tags = {
+  programmer: "developer", 
+  developer: "developer",
+  dev: "developer",
+  coder: "developer",
+  sound: "music",
+  audio: "music",
+  musician: "music",
+  music: "music",
+  composer: "music", 
+  sfx: "music",
+  artist: "artist", 
+  voice: "voice actor", 
+  actor: "voice actor",
+  va: "voice actor",
+  narrative: "writer",
+  writer: "writer",
+  playtester: "playtester"
+};
